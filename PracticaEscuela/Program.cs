75c31b7a9a4e44f7a2560dc964f050f7d7c7b7bc
leonardojc96 <53854074+leonardojc96@users.noreply.github.com>
2020-02-06 02:15:38 -0300

    Update Program.cs

--- PracticaEscuela/Program.cs
+++ PracticaEscuela/Program.cs
@@ -17,7 +17,7 @@ namespace PracticaEscuela
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Inicio());
-            Console.WriteLine("texto");
+            Console.WriteLine("Github");
         }
     }
 }
